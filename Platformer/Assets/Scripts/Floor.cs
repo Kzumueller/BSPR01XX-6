4a78dcafda41c3a1449fc47f0de5068d4467c1e6
Kevin Zumueller <kevin@zumueller.com>
2020-01-21 00:22:09 +0100

    added level timer, lives, and GameOver condition & scene

--- Platformer/Assets/Scripts/Floor.cs
+++ Platformer/Assets/Scripts/Floor.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class Floor : MonoBehaviour
 {
     public Transform LastCheckPoint;
+
+    public Text timeLabel;
+
+    public Text livesLabel;
+
+    public float timeForCompletion = 60f;
+
+    public int startingLives = 5;
+
+    private int lives;
+
+    private Timer timer;
+
+    private void Start() {
+        lives = startingLives;
+        timer = new Timer(timeForCompletion);
+    }
+
+    private void FixedUpdate() {
+        if (timer.Finished(Time.deltaTime))
+            GameOver();
+        else
+            timeLabel.text = String.Format("Time: {0:0.0}", timer.time);
+    }
 
     // moves the player to the last checkpoint, i.e. the last platform touched
     // short iteration times, baby!
     private void OnCollisionEnter(Collision collision) {
         if (!collision.gameObject.CompareTag("Player")) return;
 
-        collision.gameObject.transform.position = LastCheckPoint.position + new Vector3(0f, .25f);
+        livesLabel.text = $"Lives: {--lives}";
+
+        if (0 < lives)
+            collision.gameObject.transform.position = LastCheckPoint.position + new Vector3(0f, .25f);
+        else
+            GameOver();
+    }
+
+    public void GameOver() {
+        SceneManager.LoadScene("GameOver");
     }
 }
--- Platformer/Assets/Scripts/Platform.cs
+++ Platformer/Assets/Scripts/Platform.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+
+    public bool bouncy = false;
+    public float bounciness = 2f;
+
     // has every moving platform unset their player reference
     // this way, the last platform the player touched continues updating their position while airborne,
     // thus seemingly conserving momentum
@@ -11,5 +15,8 @@ public class Platform : MonoBehaviour
 
         FindObjectsOfType<MovingPlatform>().ToList().ForEach(platform => platform.UnsetReference());
         GameObject.FindGameObjectWithTag("Floor").GetComponent<Floor>().LastCheckPoint = transform;
+
+        if (bouncy)
+            collision.gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0f, 5f * bounciness);
     }
 }
